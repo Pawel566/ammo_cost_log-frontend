152bdbc10ceb8b61e7e476182a5b5bdf98da23d3
Pawel566 <pawel.garbaty@onet.pl>
2025-11-15 12:19:04 +0100

    update 0.3.5 User menu

---
+++ src/pages/MaintenancePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MaintenancePage = () => {
+  return (
+    <div className="card">
+      <h2>Konserwacja</h2>
+    </div>
+  );
+};
+
+export default MaintenancePage;
+
