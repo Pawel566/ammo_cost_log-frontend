2df11b1bfd8ce1cb636d97816f6522938cc17ba3
Pawel566 <pawel.garbaty@onet.pl>
2025-11-17 16:00:46 +0100

    Add maintenance-repair

--- src/pages/MyWeaponsPage.jsx
+++ src/pages/MyWeaponsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; // test123
 import { useNavigate } from 'react-router-dom';
 import { gunsAPI, attachmentsAPI, sessionsAPI, ammoAPI, maintenanceAPI } from '../services/api';
 
